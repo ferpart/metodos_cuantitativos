d9950a320e11bb619feab7152d39c9508b7fa63e
Fernando Partida <ferpart@hotmail.com>
2020-02-14 06:49:22 -0600

    Modified order of functions

--- 10_days_of_statistics/day_2/probability_functions.py
+++ 10_days_of_statistics/day_2/probability_functions.py
@@ -1,96 +1,111 @@
+"""This is a class created just for using probability functions on the 2nd day
+   of the 10_days_of_statistics course"""
+
 from itertools import product
 from itertools import permutations
 from fractions import Fraction
-from math import pow
 
-def dice_probability_fraction(n :int, x :int):
-    if (n!=0):
-        a=0
-        s= int(pow(6, n))
-        dice = [1,2,3,4,5,6]
-        dice_combinations = list(product(dice, repeat=n))
+def dice_probability_fraction(n_elements: int, x_max_sum: int):
+    """Method used to display in fraction format the probability of obtaining
+       a maximum of x_max_sum cards"""
+    if n_elements != 0:
+        a_top_fraction = 0
+        s_bot_fraction = int(pow(6, n_elements))
+        dice = [1, 2, 3, 4, 5, 6]
+        dice_combinations = list(product(dice, repeat=n_elements))
 
-        for i in range(s):
-            if (sum(dice_combinations[i])<=x):
-                a+=1
+        for i in range(s_bot_fraction):
+            if sum(dice_combinations[i]) <= x_max_sum:
+                a_top_fraction += 1
 
-        return (Fraction(a,s))
-    return (0)
+        return Fraction(a_top_fraction, s_bot_fraction)
+    return 0
 
-def dice_probability(n :int,x :int):
-    if (n!=0):
-        a=0
-        s= int(pow(6, n))
+def dice_probability(n_elements: int, x_max_sum: int):
+    """Method used to display in decimal format the probability of obtaining
+       a maximum of x_max_sum cards"""
+    if n_elements != 0:
+        a_top_fraction = 0
+        s_bot_fraction = int(pow(6, n_elements))
 
-        dice = [1,2,3,4,5,6]
-        dice_combinations = list(product(dice, repeat=n))
+        dice = [1, 2, 3, 4, 5, 6]
+        dice_combinations = list(product(dice, repeat=n_elements))
 
-        for i in range(s):
-            if (sum(dice_combinations[i])<=x):
-                a+=1
+        for i in range(s_bot_fraction):
+            if sum(dice_combinations[i]) <= x_max_sum:
+                a_top_fraction += 1
 
-        return (a/s)
-    return (0)
+        return a_top_fraction/s_bot_fraction
+    return 0
 
-def dice_probability_unique(n :int, x :int):
-    if (n!=0):
-        a=0
-        s=int(pow(6, n))
+def dice_probability_unique_fraction(n_elements: int, x_sum: int):
+    """Method used to display in fraction format the probability of obtaining
+    exactly x_sum sum of cards cards"""
+    if n_elements != 0:
+        a_top_fraction = 0
+        s_bot_fraction = int(pow(6, n_elements))
 
-        dice = [1,2,3,4,5,6]
-        dice_combinations = list(permutations(dice, n))
+        dice = [1, 2, 3, 4, 5, 6]
+        dice_combinations = list(permutations(dice, n_elements))
+        num_dice_combinations = len(dice_combinations)
 
-        for i in range(len(dice_combinations)):
-            if (sum(dice_combinations[i])==x):
-                a+=1
+        for i in range(num_dice_combinations):
+            if sum(dice_combinations[i]) == x_sum:
+                a_top_fraction += 1
 
-        return (a/s)
-    return (0)
+        return Fraction(a_top_fraction, s_bot_fraction)
+    return 0
 
-def dice_probability_unique_fraction(n :int, x :int):
-    if (n!=0):
-        a=0
-        s=int(pow(6, n))
-        
-        dice = [1,2,3,4,5,6]
-        dice_combinations = list(permutations(dice, n))
+def dice_probability_unique(n_elements: int, x_sum: int):
+    """Method used to display in decimal format the probability of obtaining
+       exactly x_sum sum of cards cards"""
+    if n_elements != 0:
+        a_top_fraction = 0
+        s_bot_fraction = int(pow(6, n_elements))
 
-        for i in range(len(dice_combinations)):
-            if (sum(dice_combinations[i])==x):
-                a+=1
+        dice = [1, 2, 3, 4, 5, 6]
+        dice_combinations = list(permutations(dice, n_elements))
+        num_dice_combinations = len(dice_combinations)
 
-        return (Fraction(a,s))
-    return (0)
+        for i in range(num_dice_combinations):
+            if sum(dice_combinations[i]) == x_sum:
+                a_top_fraction += 1
 
-def urn_probability(color :str, l :list):
-    r = l.count("r")
-    b = l.count("b")
-    if (color == "r"):
-        return (r/len(l))
-    elif (color == "b"):
-        return (b/len(l))
-    else:
-        return
+        return a_top_fraction/s_bot_fraction
+    return 0
 
-def urn_probability_fraction(color :str, l :list):
-    r = l.count("r")
-    b = l.count("b")
-    if (color == "r"):
-        return (Fraction(r, len(l)))
-    elif (color == "b"):
-        return (Fraction(b, len(l)))
-    else:
-        return
+def urn_probability(color: str, list_balls: list):
+    """Calculates the probability in decimal of getting a specific color of ball
+    in an urn from a list of colored balls"""
+    red_count = list_balls.count("r")
+    blk_count = list_balls.count("b")
+    if color == "r":
+        return red_count/len(list_balls)
+    if color == "b":
+        return blk_count/len(list_balls)
+    return 0
 
-def urn_probability_of_retrieval(chain_to_find : list, x :list, y :list, z:list):
+def urn_probability_fraction(color: str, list_balls: list):
+    """Calculates the probability in fraction of getting a specific color of ball
+    in an urn from a list of colored balls"""
+    red_count = list_balls.count("r")
+    blk_count = list_balls.count("b")
+    if color == "r":
+        return Fraction(red_count, len(list_balls))
+    if color == "b":
+        return Fraction(blk_count, len(list_balls))
+    return 0
+
+def urn_probability_of_retrieval(chain_to_find: list, x_list: list, y_list: list, z_list: list):
     """Calculates the probability of a specific chain to be found within 3 lists
     """
-    if (len(chain_to_find)!=0 or len(x)!=0 or len(y)!=0 or len(z)!=0):
+    if (len(chain_to_find) != 0 or len(x_list) != 0 or len(y_list) != 0 or
+            len(z_list) != 0):
 
-        a= urn_probability_fraction(chain_to_find[0], x)
-        b= urn_probability_fraction(chain_to_find[1], y)
-        c= urn_probability_fraction(chain_to_find[2], z)
+        first_pull = urn_probability_fraction(chain_to_find[0], x_list)
+        second_pull = urn_probability_fraction(chain_to_find[1], y_list)
+        third_pull = urn_probability_fraction(chain_to_find[2], z_list)
 
-        final_probability = a*b*c
-        return (final_probability)
-    return (0)
+        final_probability = first_pull*second_pull*third_pull
+        return final_probability
+    return 0
