922ebea2fd71ca69b6350d90506f3acc5f3468f3
Fernando Partida <ferpart@hotmail.com>
2020-02-20 11:29:28 -0600

    day_9 code updated

--- 10_days_of_statistics/day_9/multiple_linear_regression.py
+++ 10_days_of_statistics/day_9/multiple_linear_regression.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
 """Code for solving the Multiple Linear Regression problem"""
 
-from sklearn import linear_model
+from lib import sklearn
 
-first = list(map(int, str.split(input(), " ")))
-m, n = first[0], first[1]
+def main():
+    """main function for the method"""
 
-data = [list(float(x) for x in input().split()) for i in range(n)]
+    initial_list = list(map(int, str.split(input(), " ")))
+    m_value, n_value = initial_list[0], initial_list[1]
 
-x = [[item[i] for i in range(m)] for item in data]
-y = [item[-1] for item in data]
-lm = linear_model.LinearRegression()
-lm.fit(x, y)
-a = lm.intercept_
-b = lm.coef_
+    data = [list(float(x) for x in input().split()) for i in range(n_value)]
 
-for i in range(int(input())):
-    data = list(map(float, input().split()))
-    ans = [b[j]*data[j] for j in range(m)]
-    print(a+sum(ans))
+    x_list = [[item[i] for i in range(m_value)] for item in data]
+    y_list = [item[-1] for item in data]
+    lm_model = sklearn.linear_model.LinearRegression()
+    lm_model.fit(x_list, y_list)
+    a_intercept = lm_model.intercept_
+    b_coefficient = lm_model.coef_
+
+    for _ in range(int(input())):
+        data = list(map(float, input().split()))
+        ans = [b_coefficient[j]*data[j] for j in range(m_value)]
+        print(a_intercept+sum(ans))
+
+if __name__ == "__main__":
+    main()
