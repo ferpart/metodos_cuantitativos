7d6baa8eb49bf803a926dbc08abf3f8620d57928
Fernando Partida <ferpart@hotmail.com>
2020-06-20 10:01:02 -0500

    idk

--- homeworks/generadores_de_numeros/lineal_congruency_method.py
+++ homeworks/generadores_de_numeros/lineal_congruency_method.py
@@ -13,9 +13,9 @@ def main():
     """main method for the class"""
 
     x_value = 7         # x initial value for the method
-    a_multiplier = 1    # a multiplier for the method
+    a_multiplier = 7    # a multiplier for the method
     c_addition = 7      # c additive for the method
-    m_modulus = 13      # m modulus for the method
+    m_modulus = 10      # m modulus for the method
 
     for _ in range(16):
         x_value = generator(x_value, a_multiplier, c_addition, m_modulus)
